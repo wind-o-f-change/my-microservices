053d802c3658814cba92061cb7fa999e65cced63
Evtushenko Anton <Anton31>
2021-02-01 01:26:17 +0300

    Start create the getaway API

---
+++ getaway/src/main/java/com/example/getaway/GetawayApplication.java
@@ -0,0 +1,13 @@
+package com.example.getaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetawayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetawayApplication.class, args);
+    }
+
+}
--- magazine-service/src/main/java/com/example/magazineservice/Controller.java
+++ magazine-service/src/main/java/com/example/magazineservice/Controller.java
@@ -1,12 +1,13 @@
 package com.example.magazineservice;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping
