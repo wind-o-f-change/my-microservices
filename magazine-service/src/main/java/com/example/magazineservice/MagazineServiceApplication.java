881ea9e6e7efde45341cf6684e885e4ca9f1cb6f
Evtushenko Anton <Anton31>
2021-02-02 23:22:16 +0300

    рабочее, но еще сырое

--- magazine-service/src/main/java/com/example/magazineservice/MagazineServiceApplication.java
+++ magazine-service/src/main/java/com/example/magazineservice/MagazineServiceApplication.java
@@ -1,12 +1,8 @@
 package com.example.magazineservice;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
